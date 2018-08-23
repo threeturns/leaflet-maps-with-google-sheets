091659e144a50101b3bc2df41f7f6035dfcd6318
threeturns <42554473+threeturns@users.noreply.github.com>
2018-08-23 10:45:22 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1uh_PfjDQwYBOTY42UYw3rN6TONnSvzYmnE2w3I52k9M/edit#gid=0';
